ac6c3000736efc7d54bca201f7bd7467753a1a4b
Rain <rainkalugdan@gmail.com>
2025-10-08 18:32:21 +0800

    feat(home): set html structure for Home

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,12 +1,14 @@
-export default function Home() {
+export default function Home({ onStart }) {
   return (
-    <>
-    <div className="quiz">
-        
-          <h1 className="quiz__title">Quizzical</h1>
-          <p className="quiz__description">Some description if needed</p>
-          <button className="quiz__start-button">Start quiz</button>
+    <div className="home">
+      <h1 className="home__title">Quizzical</h1>
+      <p className="home__description">Some description if needed</p>
+      <button
+        className="home__start-button"
+        onClick={() => onStart && onStart()}
+      >
+        Start quiz
+      </button>
     </div>
-    </>
   );
 }
