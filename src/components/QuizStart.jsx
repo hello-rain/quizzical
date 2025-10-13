509ce418f9bebaa10c7fa0cd578890fb7e51e5e6
Rain <rainkalugdan@gmail.com>
2025-10-13 15:22:39 +0800

    refactor(quiz-start): change div to section

--- src/components/QuizStart.jsx
+++ src/components/QuizStart.jsx
@@ -4,12 +4,12 @@ export default function QuizStart({
   disabled = false,
 }) {
   return (
-    <div className="quiz-start">
+    <section className="quiz-start">
       <h1 className="quiz-start__title">Quizzical</h1>
       <p className="quiz-start__description">{introText}</p>
       <button className="btn" onClick={onStart} disabled={disabled}>
         Start quiz
       </button>
-    </div>
+    </section>
   );
 }
