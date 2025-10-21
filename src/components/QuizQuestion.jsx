0876027bf79495ec8d2faffaf61e28fb3125b6c7
Rain <rainkalugdan@gmail.com>
2025-10-21 12:18:48 +0800

    refactor(utils): move utils to utils/

--- src/components/QuizQuestion.jsx
+++ src/components/QuizQuestion.jsx
@@ -2,24 +2,8 @@
 
 import { useMemo } from "react";
 import clsx from "clsx";
-
-// Fisher-Yates shuffle - non-mutating: returns a new shuffled array
-function shuffleArray(arr) {
-  const newArr = arr.slice();
-  for (let i = newArr.length - 1; i > 0; i--) {
-    const j = Math.floor(Math.random() * (i + 1));
-    [newArr[i], newArr[j]] = [newArr[j], newArr[i]];
-  }
-  return newArr;
-}
-
-// Decode HTML entities returned by the API into plain text
-function decodeHtml(html) {
-  if (typeof document === "undefined") return html;
-  const text = document.createElement("textarea");
-  text.innerHTML = html;
-  return text.value;
-}
+import decodeHtml from "../utils/html";
+import shuffleArray from "../utils/shuffleArray";
 
 function QuizQuestion({ question, questionIndex, selected, onAnswer }) {
   if (!question) return null;
